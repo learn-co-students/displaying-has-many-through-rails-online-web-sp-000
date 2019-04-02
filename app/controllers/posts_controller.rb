07e9aabcc230b86039565ab28c9ed47d31f15985
jasonodoom <jasonodoom@riseup.net>
2019-04-02 16:24:44 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,33 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to @post
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    Post.find(params[:id]).update(post_params)
+    redirect_to post_path
+  end
+
+  def destroy
+    Post.destroy(params[:id])
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
