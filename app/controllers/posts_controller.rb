ecaf11403ac5635c196f061bfc296d62d14e2b58
Rafael Batista <RafaelB24@yahoo.com>
2019-04-08 16:08:30 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,10 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  private
+
+  def post_params(*args)
+    params.require(:post).permit(*args)
+  end
 end
