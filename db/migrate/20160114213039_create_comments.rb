15a70cf5d99830a55ecdac32d79d3a794b49d5dd
Alan Krajina <alankrajina88@gmail.com>
2019-08-15 10:28:46 +0000

    Done.

--- db/migrate/20160114213039_create_comments.rb
+++ db/migrate/20160114213039_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string :content
-      t.belongs_to :user
-      t.belongs_to :post
+    t.belongs_to :user
+    t.belongs_to :post
       t.timestamps null: false
     end
   end
