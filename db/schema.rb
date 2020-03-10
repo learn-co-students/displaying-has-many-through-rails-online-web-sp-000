c7ee4970cdf9cc5bfcf0ce2bce0eb9427a1de3ef
azbury <acasbury25@aol.com>
2020-03-10 19:52:31 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
+#hello
 ActiveRecord::Schema.define(version: 20160114213039) do
 
   create_table "comments", force: :cascade do |t|
