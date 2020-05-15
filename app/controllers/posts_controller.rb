f91a3b40ad06307a6cd68eb896ce7041ae1863fe
doakcovington <doakcovington@protonmail.com>
2020-05-15 11:11:14 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
-class PostsController < ApplicationController
+# class PostsController < ApplicationController
 
-  def show
-    @post = Post.find(params[:id])
-  end
-end
+#   def show
+#     @post = Post.find(params[:id])
+#   end
+# end
