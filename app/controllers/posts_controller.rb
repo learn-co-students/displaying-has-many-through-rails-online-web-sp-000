441b46de5262bb2ef4987e9c4317e3954959bb35
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-04 02:27:37 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
 
+
+#display all comments on a particular post
+#finds a post and makes it available for display
   def show
     @post = Post.find(params[:id])
   end
--- db/migrate/20160114213006_create_posts.rb
+++ db/migrate/20160114213006_create_posts.rb
@@ -7,3 +7,11 @@ class CreatePosts < ActiveRecord::Migration
     end
   end
 end
+
+
+
+#We can set up a many-to-many relationship using a join table. In this case, comments will act as our join table. Any table that contains two foreign keys can be thought of as a join table. A row in our comments table will look something like this:
+# id	     content	     post_id	user_id
+# 1	 "I loved this post!"	5	      3
+
+#For this instance, we know that the Comment with an ID of 1 was created by the User with an ID of 3 for the Post with an ID of 5
